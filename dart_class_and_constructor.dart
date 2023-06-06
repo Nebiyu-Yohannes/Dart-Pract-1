8aa77180259a02636deb1a2f4ad01a4c0be9f5d2
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-06-06 15:33:44 +0300

    classes and constructor

--- dart_class_and_constructor.dart
+++ dart_class_and_constructor.dart
@@ -1,12 +1,31 @@
 
 void main(){
+  Student studentOne = Student('nebicho', 23, true);
+  print(studentOne.name);
+  print(studentOne.age);
+  studentOne.welcome();
+
+  Student studentTwo = Student('numa', 21, false);
+  print(studentTwo.name);
+  print(studentTwo.age);
+  studentTwo.welcome();
 
 }
 
 class Student {
 
-  String name = 'Nebicho';
-  int age = 23;
+  late String name;
+  late int age;
+  late bool car;
+
+// creating a constructor, a function that runs when a class is initiated
+// parameters of our class instances link to the class's parameter  
+
+  Student(n, a, c){
+    this.name = n;
+    this.age = a;
+    this.car = c;
+  } // saying the instances we create will be assigned as a parameter
 
   void welcome(){
     print('Welcome Back');
