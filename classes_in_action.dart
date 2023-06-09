7c92f633ec7d6abf90e20f5955499b65dbda4f4b
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-06-09 15:23:59 +0300

    .smsing

---
+++ classes_in_action.dart
@@ -0,0 +1,34 @@
+// this is the implementation of classses and constructors in dart programming language
+
+// the main function
+
+void main(){
+
+  // execute here
+  Animals dog = Animals("Dog", "dogeiciance", true, 6);
+
+  print(dog.common_name);
+
+
+
+}
+
+
+class Animals{
+  // creating the properties for the class animals
+
+  String common_name;
+  String scientific_name;
+  bool domesticated;
+  int age;
+
+  Animals(String c_name, String s_name, bool domestic, int age){
+    this.common_name = c_name;
+    this.scientific_name = s_name;
+    this.domesticated = domestic;
+    this.age = age; 
+  }
+
+
+
+}
